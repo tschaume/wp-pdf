42980e33da2b4ab858baf4d36c089111d6ac67cc
Patrick Huck <phuck@lbl.gov>
2013-03-01 18:02:47 -0500

    add more content for note

--- split.sh
+++ split.sh
@@ -3,8 +3,10 @@
 input="userguide.txt"
 tmp="tmp.txt"
 filelist="filelist.txt"
-lvl1="42 63 124 197 401 409 502"
-#lvl1="$lvl1 521 900 939 1162 1229 1250 1275 1338 1503 2254"
+lvl1="42 63 124 197 401 409 502 881 920 1143 1210 1231 1256 1320 1485 1831 2236 2271"
+lvl1="$lvl1 2300 2419 2981 3054 3540 3615 3695 4123 4149 4282 4345 4555 4679 4960"
+lvl1="$lvl1 5084 5107 5146"
+#"5501 5525 5551"
 
 if [ -d out ]; then rm -rf out; fi
 mkdir out
@@ -12,20 +14,20 @@ if [ -e $filelist ]; then rm $filelist; fi
 
 prev=1
 for h in $lvl1; do
-  echo "-------------------------------------------"
   sed -n $prev,${h}p $input >> $tmp
   line=3 # default header line
   title=`sed -n ${line}p $tmp`
   if [[ "$title" == *"[["* ]]; then line=4; title=`sed -n ${line}p $tmp`; fi
   filename=`echo $title | sed 's: ::g'`
   out="out/$filename.txt"
+  echo $out
   echo -n "$out " >> $filelist
   sed $(($line+1))d $tmp | sed "${line}s:^${title}://== ${title}:" | sed 1,2d >> ${tmp}_2
   echo >> $out
   echo ":blogpost-title: ${title}" >> $out
   echo ":blogpost-posttype: page" >> $out
   cat ${tmp}_2 >> $out
-  cat $out | head -5
+  #cat $out | head -5
   rm $tmp*
   prev=$h
 done
