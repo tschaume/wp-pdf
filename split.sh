86c9a5bfe4b7496600e7dddd789224c2ecb8a3b1
Patrick Huck <phuck@lbl.gov>
2013-03-01 17:50:23 -0500

    implement split.sh

--- split.sh
+++ split.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
 
-lvl1="1 22 61 82 143 216 420 428 521 900 939 1162 1229 1250 1275 1338 1503 2254"
-lvl1="$lvl1 2289 2318 2437 2999 3072 3558 3633 3713 4141 4167 4300 4363 4573"
-lvl1="$lvl1 4696 4977 5101 5124 5163 5518 5542 5568 5577 5608 5639 5665 5710"
-lvl1="$lvl1 5809 6009"
+input="userguide.txt"
+tmp="tmp.txt"
+filelist="filelist.txt"
+lvl1="42 63 124 197 401 409 502"
+#lvl1="$lvl1 521 900 939 1162 1229 1250 1275 1338 1503 2254"
+
+if [ -d out ]; then rm -rf out; fi
+mkdir out
+if [ -e $filelist ]; then rm $filelist; fi
+
+prev=1
+for h in $lvl1; do
+  echo "-------------------------------------------"
+  sed -n $prev,${h}p $input >> $tmp
+  line=3 # default header line
+  title=`sed -n ${line}p $tmp`
+  if [[ "$title" == *"[["* ]]; then line=4; title=`sed -n ${line}p $tmp`; fi
+  filename=`echo $title | sed 's: ::g'`
+  out="out/$filename.txt"
+  echo -n "$out " >> $filelist
+  sed $(($line+1))d $tmp | sed "${line}s:^${title}://== ${title}:" | sed 1,2d >> ${tmp}_2
+  echo >> $out
+  echo ":blogpost-title: ${title}" >> $out
+  echo ":blogpost-posttype: page" >> $out
+  cat ${tmp}_2 >> $out
+  cat $out | head -5
+  rm $tmp*
+  prev=$h
+done
+
