896c8283f5fadd2c1eba1baad7e50e581c0a2fd6
Patrick Huck <phuck@lbl.gov>
2013-03-04 15:50:21 -0800

    split.sh working

--- split.sh
+++ split.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Copyright (c) 2013 Patrick Huck
 
 input="userguide.txt"
 tmp="tmp.txt"
@@ -9,6 +10,19 @@ lvl1="42 64 125 198 402 410 503 882 921"
 #lvl1="$lvl1 2300 2419 2981 3054 3540 3615 3695 4123 4149 4282 4345 4555 4679 4960"
 #lvl1="$lvl1 5084 5107 5146"
 #"5501 5525 5551"
+
+appendFile() { # filename
+  echo $1
+  echo -n "$1 " >> $filelist
+}
+
+getSplitLine() { # ~~~line tmp
+  local splitline=$(($1-2))
+  if [[ "`sed -n ${splitline}p $2`" == *"[["* ]]; then
+    splitline=$(($splitline-1))
+  fi
+  echo $splitline
+}
 
 writeHeader() { # line, title, filename, tmp, levelstring
   sed $(($1+1))d $4 | sed "$1s:^$2://$5 $2:" >> xxx
@@ -25,6 +39,7 @@ if [ -e $filelist ]; then rm $filelist; fi
 
 prev=1
 for h in $lvl1; do
+
   sed -n $prev,${h}p $input >> $tmp
   line=3 # default header line
   title=`sed -n ${line}p $tmp`
@@ -32,43 +47,50 @@ for h in $lvl1; do
   dirname=`echo $title | sed 's: ::g'`
   outdir=out/$dirname
   mkdir $outdir
-  if grep -qn '^~~~' $tmp; then
+
+  if grep -qn '^~~~' $tmp; then # sub-titles
+
     ln=`grep -n '^~~~' $tmp | awk -F: '{print $1}' | tr '\n' ' '`
-    prevv=1
-    echo $ln
+    #ln="$ln "`cat $tmp | wc -l`
+
+    # level 1 title
+    filename=$outdir/$dirname.txt
+    appendFile $filename
+    sl=$(getSplitLine `echo $ln | awk '{print $1}'` $tmp)
+    sed -n 1,${sl}p $tmp >> ${tmp}_2
+    writeHeader $line "$title" $filename ${tmp}_2 '=='
+    rm ${tmp}_2
+
+    # level 2 titles
+    fld=2
     for l in $ln; do
-      if [ $prevv -eq 1 ]; then # level 1 title
-        filename=$outdir/$dirname.txt
-      else # level 2 title
-        echo $prevv
-        subtitle=`sed -n ${prevv}p $tmp` # subtitle doesn't fit file content
-        # need previous line
-        filename=$outdir/${dirname}_`echo ${subtitle} | sed 's: ::g'`.txt
+      subtitle=`sed -n $(($l-1))p $tmp`
+      filename=$outdir/${dirname}_`echo ${subtitle} | sed 's: ::g'`.txt
+      appendFile $filename
+
+      sl1=$(getSplitLine $l $tmp)
+      nl=$(echo $ln | awk "{print \$$fld}") # next ~~~~line
+      if [ -z "$nl" ]; then
+        nl=$(cat $tmp | wc -l | sed 's: ::g')
+        let nl=$nl+2
       fi
-      echo $filename
-      echo -n "$filename " >> $filelist
-      curline=$(($l-2))
-      if [[ "`sed -n ${curline}p $tmp`" == *"[["* ]]; then
-        curline=$(($curline-1))
-      fi
-      sed -n $prevv,${curline}p $tmp >> ${tmp}_2
-      if [ $(($prevv-1)) -eq 0 ]; then # level 1 title
-        writeHeader $line "$title" $filename ${tmp}_2 '=='
-      else # level 2 title
-        #writeHeader $line "$subtitle" $filename ${tmp}_2 '==='
-        cp ${tmp}_2 $filename
-      fi
+      sl2=$(getSplitLine $nl $tmp)
+      sed -n $sl1,${sl2}p $tmp >> ${tmp}_2
+      let fld=$fld+1 # next field for awk
+
+      subline=2 # default sub-header line
+      if [[ "$(sed -n ${subline}p ${tmp}_2)" == *"[["* ]]; then subline=3; fi
+      writeHeader $subline "$subtitle" $filename ${tmp}_2 '==='
+      #cp ${tmp}_2 $filename
       rm ${tmp}_2
-      prevv=$curline
     done
+
   else # no level 2 title found
     filename=$outdir/$dirname.txt
-    echo $filename
-    echo -n "$filename " >> $filelist
+    appendFile $filename
     writeHeader $line "$title" $filename $tmp '=='
   fi
 
   rm $tmp
   prev=$h
 done
-
